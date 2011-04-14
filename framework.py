85b12a94b0e745cd69912368b1f4843df5ccc0c6
Jonathan Hartley <tartley@tartley.com>
2011-04-15 00:36:54 +0100

    t01 works

---
+++ framework.py
@@ -0,0 +1,12 @@
+
+from OpenGL import GL
+from OpenGL.GL.ARB import vertex_array_object
+
+def glGenVertexArray():
+    '''
+    Return the integer ID of the created vertex object array
+    '''
+    vao_id = GL.GLuint(0)
+    vertex_array_object.glGenVertexArrays(1, vao_id)
+    return vao_id.value
+
---
+++ t01-hello-triangle.py
@@ -0,0 +1,95 @@
+
+from ctypes import c_void_p
+from OpenGL import GL
+from OpenGL.GL.shaders import compileShader, compileProgram
+from OpenGL.GL.ARB.vertex_array_object import glBindVertexArray
+from framework import glGenVertexArray
+import pyglet
+
+# This is lame. What's the right way to get the sizeof(GLfloat) ?
+# Tried sys.getsizeof(GLfloat), sys.getsizeof(GLfloat()),
+# GLfloat().__sizeof__(). All give different wrong answers (size of python
+# objects, not of underlying C 'float' type)
+sizeOfFloat = 4
+
+vertexPositions = [
+     0.75,  0.75,  0.0,  1.0,
+     0.75, -0.75,  0.0,  1.0,
+    -0.75, -0.75,  0.0,  1.0,
+]
+
+strVertexShader = """
+in vec4 position;
+void main()
+{
+   gl_Position = position;
+}
+"""
+strFragmentShader = """
+out vec4 outputColor;
+void main()
+{
+   outputColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+}
+"""
+
+
+window = None
+theProgram = None
+positionBufferObject = None
+
+
+def initialize_program():
+    global theProgram
+    theProgram = compileProgram(
+        compileShader(strVertexShader, GL.GL_VERTEX_SHADER),
+        compileShader(strFragmentShader, GL.GL_FRAGMENT_SHADER)
+    )
+
+
+def initialize_vertex_buffer():
+    global positionBufferObject
+    positionBufferObject = GL.glGenBuffers(1)
+    GL.glBindBuffer(GL.GL_ARRAY_BUFFER, positionBufferObject)
+    array_type = (GL.GLfloat * len(vertexPositions))
+    GL.glBufferData(
+        GL.GL_ARRAY_BUFFER, len(vertexPositions) * sizeOfFloat,
+        array_type(*vertexPositions), GL.GL_STATIC_DRAW
+    )
+    GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
+
+
+def init():
+    initialize_program()
+    initialize_vertex_buffer()
+    glBindVertexArray( glGenVertexArray() )
+
+
+def display():
+    GL.glClearColor(0.0, 0.0, 0.0, 0.0)
+    GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+    GL.glUseProgram(theProgram)
+    GL.glBindBuffer(GL.GL_ARRAY_BUFFER, positionBufferObject)
+    GL.glEnableVertexAttribArray(0)
+    GL.glVertexAttribPointer(0, 4, GL.GL_FLOAT, False, 0, c_void_p(0))
+    GL.glDrawArrays(GL.GL_TRIANGLES, 0, 3)
+    GL.glDisableVertexAttribArray(0)
+    GL.glUseProgram(0)
+
+
+def reshape(width, height):
+    GL.glViewport(0, 0, width, height)
+
+
+def main():
+    global window
+    window = pyglet.window.Window()
+    window.on_draw = display
+    window.on_resize = reshape
+    init()
+    pyglet.app.run()
+
+
+if __name__ == '__main__':
+    main()
+
