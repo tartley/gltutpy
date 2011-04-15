479c1ada52bf80e1fde8451fbeca14f23218c054
Jonathan Hartley <tartley@tartley.com>
2011-04-15 09:25:02 +0100

    yet more comments

--- framework.py
+++ framework.py
@@ -2,10 +2,10 @@
 from OpenGL import GL
 from OpenGL.GL.ARB import vertex_array_object
 
-# I'm not entirely sure what I have to go fishing around inside OpenGL.GL.ARB
-# to find this function, nor why PyOpenGL doesn't provide a more pythonic
-# wrapper for it. I suspect it might be because I only have OpenGL2.1 instead
-# of 3.0 or greater.
+# I'm not entirely sure why I have to look inside OpenGL.GL.ARB
+# to find this function, nor why PyOpenGL doesn't provide its usual pythonic
+# wrapper for it. I suspect it's because I only have OpenGL2.1 hardware,
+# instead of 3.0 or greater.
 def glGenVertexArray():
     '''
     Return the integer ID of the created vertex object array
--- t01-hello-triangle.py
+++ t01-hello-triangle.py
@@ -46,10 +46,10 @@ void main()
 
 window = None
 
-# integer handle identifying our compiled shader program
+# Integer handle identifying our compiled shader program
 theProgram = None
 
-# integer handle identifying the GPU memory storing our vertex position array
+# Integer handle identifying the GPU memory storing our vertex position array
 positionBufferObject = None
 
 
@@ -78,12 +78,16 @@ def initialize_vertex_buffer():
     GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
 
 
+# Called once at application start-up.
+# Must be called after we have an OpenGL context, i.e. after the pyglet
+# window is created
 def init():
     initialize_program()
     initialize_vertex_buffer()
     glBindVertexArray( glGenVertexArray() )
 
 
+# Called to redraw the contents of the window
 def display():
     GL.glClearColor(0.0, 0.0, 0.0, 0.0)
     GL.glClear(GL.GL_COLOR_BUFFER_BIT)
@@ -99,6 +103,7 @@ def display():
     GL.glUseProgram(0)
 
 
+# Called when the window is resized, including once at application start-up
 def reshape(width, height):
     GL.glViewport(0, 0, width, height)
 
