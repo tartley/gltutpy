6cf8207240f81cfd0fd97af9f425cd970a978b86
Jonathan Hartley <tartley@tartley.com>
2011-04-20 13:53:49 +0100

    massive directory reorg, to match source material

--- t02.playing-with-colors/VertexColors.py
+++ t02.playing-with-colors/VertexColors.py
@@ -1,5 +1,6 @@
 
 from ctypes import c_void_p
+from os.path import dirname, join
 
 from OpenGL import GL
 from OpenGL.GL.shaders import compileShader, compileProgram
@@ -18,10 +19,13 @@ null = c_void_p(0)
 sizeOfFloat = 4
 
 # Three vertices, with an x,y,z & w for each.
-vertexPositions = [
-     0.75,  0.75,  0.0,  1.0,
-     0.75, -0.75,  0.0,  1.0,
-    -0.75, -0.75,  0.0,  1.0,
+vertexData = [
+     0.0,  0.5,   0.0, 1.0,
+     0.5, -0.366, 0.0, 1.0,
+    -0.5, -0.366, 0.0, 1.0,
+     1.0,  0.0,   0.0, 1.0,
+     0.0,  1.0,   0.0, 1.0,
+     0.0,  0.0,   1.0, 1.0,
 ]
 vertexComponents = 4
 
@@ -32,11 +36,11 @@ window = None
 theProgram = None
 
 # Integer handle identifying the GPU memory storing our vertex position array
-positionBufferObject = None
+vertexBufferObject = None
 
 
 def loadFile(filename):
-    with open(filename) as fp:
+    with open(join(dirname(__file__), filename)) as fp:
         return fp.read()
 
 
@@ -49,20 +53,20 @@ def initialize_program():
     global theProgram
     theProgram = compileProgram(
         compileShader(
-            loadFile('shaders/FragPosition.vert'), GL.GL_VERTEX_SHADER),
+            loadFile('VertexColors.vert'), GL.GL_VERTEX_SHADER),
         compileShader(
-            loadFile('shaders/FragPosition.frag'), GL.GL_FRAGMENT_SHADER)
+            loadFile('VertexColors.frag'), GL.GL_FRAGMENT_SHADER)
     )
 
 
 def initialize_vertex_buffer():
-    global positionBufferObject
-    positionBufferObject = GL.glGenBuffers(1)
-    GL.glBindBuffer(GL.GL_ARRAY_BUFFER, positionBufferObject)
-    array_type = (GL.GLfloat * len(vertexPositions))
+    global vertexBufferObject
+    vertexBufferObject = GL.glGenBuffers(1)
+    GL.glBindBuffer(GL.GL_ARRAY_BUFFER, vertexBufferObject)
+    array_type = (GL.GLfloat * len(vertexData))
     GL.glBufferData(
-        GL.GL_ARRAY_BUFFER, len(vertexPositions) * sizeOfFloat,
-        array_type(*vertexPositions), GL.GL_STATIC_DRAW
+        GL.GL_ARRAY_BUFFER, len(vertexData) * sizeOfFloat,
+        array_type(*vertexData), GL.GL_STREAM_DRAW # TODO, needs to be stream?
     )
     GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
 
@@ -82,13 +86,16 @@ def display():
     GL.glClear(GL.GL_COLOR_BUFFER_BIT)
 
     GL.glUseProgram(theProgram)
-    GL.glBindBuffer(GL.GL_ARRAY_BUFFER, positionBufferObject)
+    GL.glBindBuffer(GL.GL_ARRAY_BUFFER, vertexBufferObject)
     GL.glEnableVertexAttribArray(0)
-    GL.glVertexAttribPointer(0, vertexComponents, GL.GL_FLOAT, False, 0, null)
+    GL.glEnableVertexAttribArray(1)
+    GL.glVertexAttribPointer(1, 4, GL.GL_FLOAT, False, 0, null)
+    GL.glVertexAttribPointer(0, 4, GL.GL_FLOAT, False, 0, c_void_p(48))
 
-    GL.glDrawArrays(GL.GL_TRIANGLES, 0, len(vertexPositions) / vertexComponents)
+    GL.glDrawArrays(GL.GL_TRIANGLES, 0, 3)
 
     GL.glDisableVertexAttribArray(0)
+    GL.glDisableVertexAttribArray(1)
     GL.glUseProgram(0)
 
 
---
+++ t02.playing-with-colors/VertexColors.vert
@@ -0,0 +1,13 @@
+#version 120
+
+in vec4 position;
+in vec4 color;
+
+smooth out vec4 theColor;
+
+void main()
+{
+    gl_Position = position;
+    theColor = color;
+}
+
